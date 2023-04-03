540e932a85c86ec41aa504f0d432e7da5aee9563
sdb07khan <ksaddab@gmail.com>
2023-04-03 13:59:16 +0530

    added pin and scroll animation

--- script.js
+++ script.js
@@ -9,42 +9,6 @@ function animation() {
   TL.to(".banner__image", { rotate: "+=360", duration: 10, ease: "none" });
 }
 
-// logos marquee HIGHLIGHT
-
-// // Next, select the elements you need
-// const logoContainer = document.querySelector(".featuredTextContainer");
-// const logoContent = document.querySelector(".featuredLogoList");
-// const logoSpans = document.querySelectorAll(".featuredLogo");
-
-// // Calculate the total width of the marquee content
-// let logoContentWidth = 0;
-// logoSpans.forEach((span) => (logoContentWidth += span.offsetWidth));
-
-// // Set the width of the marquee content to be twice the total width
-// logoContent.style.width = logoContentWidth * 2 + "rem";
-
-// // Create a GSAP timeline for your animation
-// const logoAnimation = gsap.timeline({ repeat: -1 }); // Set repeat to -1 for an infinite loop
-
-// // Add animations to your timeline
-// logoAnimation.to(logoContent, {
-//   x: -logoContentWidth,
-//   duration: 10,
-//   ease: "steps(12)",
-//   repeatDelay: 0,
-// });
-
-// // Finally, create a scroll trigger to restart the animation when it ends
-// const scrollTrigger = {
-//   trigger: logoContainer,
-//   start: "0 100%", // Start the trigger when the top of the container is 100% visible
-//   end: `+=${logoContentWidth}`, // End the trigger when the bottom of the container is 500px below the top
-//   onEnter: () => logoAnimation.play(0), // Restart the animation from the beginning when it enters the trigger
-//   onLeaveBack: () => logoAnimation.pause(0), // Pause the animation when it leaves the trigger
-// };
-
-// gsap.timeline().add(scrollTrigger); // Add the scroll trigger to your timeline
-
 // two images slides on opposite direction on scroll HIGHLIGHT
 
 const tl = gsap.timeline();
@@ -59,7 +23,7 @@ ScrollTrigger.create({
   trigger: ".scrollImageOne",
   Start: "top bottom",
   end: "bottom -20%",
-  scrub: true,
+  scrub: 1,
 });
 
 const tl2 = gsap.timeline();
@@ -73,8 +37,8 @@ ScrollTrigger.create({
   animation: tl2,
   trigger: ".scrollImageTwo",
   Start: "top bottom",
-  end: "bottom -40%",
-  scrub: true,
+  end: "bottom -20%",
+  scrub: 1,
 });
 
 const tl3 = gsap.timeline();
@@ -88,8 +52,8 @@ ScrollTrigger.create({
   animation: tl3,
   trigger: ".scrollImageThree",
   Start: "top bottom",
-  end: "bottom -40%",
-  scrub: true,
+  end: "bottom -20%",
+  scrub: 1,
 });
 
 const tl4 = gsap.timeline();
@@ -103,44 +67,75 @@ ScrollTrigger.create({
   animation: tl4,
   trigger: ".scrollImageFour",
   Start: "top bottom",
-  end: "bottom -40%",
+  end: "bottom -20%",
+  scrub: 1,
+});
+
+//////////////////////////////////////////////////////////////
+// text reveal animation HIGHLIGHT
+
+// import gsap from "gsap-trial";
+
+// import { SplitText } from "gsap/SplitText";
+
+// gsap.registerPlugin(SplitText);
+
+// let mySplitText = new SplitText(".marketingStrategy__heading", {
+//   type: "chars",
+// });
+
+// let chars = mySplitText.chars;
+
+// function splitText() {
+//   const myText = document.getElementById("myText");
+//   const mySplitText = new SplitText(myText, { type: "chars" });
+// }
+
+// function animateText() {
+//   const mySplitText = new SplitText("#myText", { type: "chars" });
+//   gsap.to(mySplitText.chars, {
+//     duration: 1,
+//     opacity: 0,
+//     stagger: 0.1,
+//     ease: "power2.out",
+//   });
+// }
+
+/////////////////////////////////////////////////////////////
+// work section HIGHLIGHT (pin & horizontal scroll)
+
+// this is for the heading animation
+const myWork = gsap.timeline();
+
+myWork.to(".workHeading", {
+  scale: "0.8",
+  duration: 8,
+});
+
+ScrollTrigger.create({
+  animation: myWork,
+  trigger: ".workHeading",
+  Start: "top bottom",
   scrub: true,
 });
 
-/////////////////////////////////////////////////////
+// this is for the scroll animation
+const container = document.querySelector(".workCardSlideBox");
+const cards = gsap.utils.toArray(".workCardBox");
 
-// First, make sure you have included GSAP in your project
+let scrollTween = gsap.to(cards, {
+  xPercent: -73 * (cards.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: container,
+    pin: true,
+    scrub: 1,
+    end: "+=2000",
+  },
+});
 
-// // Next, select the elements you need
-// const marqueeContainer = document.querySelector(".runningLogo");
-// const marqueeContent = document.querySelector(".runningLogoBox");
-// const marqueeSpans = document.querySelectorAll(".allLogos");
-
-// // Calculate the total width of the marquee content
-// let marqueeContentWidth = 0;
-// marqueeSpans.forEach((span) => (marqueeContentWidth += span.offsetWidth));
-
-// // Set the width of the marquee content to be twice the total width
-// marqueeContent.style.width = marqueeContentWidth * 2 + "rem";
-
-// // Create a GSAP timeline for your animation
-// const animationTimeline = gsap.timeline({ repeat: -1 }); // Set repeat to -1 for an infinite loop
-
-// // Add animations to your timeline
-// animationTimeline.to(marqueeContent, {
-//   x: -marqueeContentWidth,
-//   duration: 10,
-//   ease: "none",
-//   repeatDelay: 0,
-// });
-
-// // Finally, create a scroll trigger to restart the animation when it ends
-// const marqueeTrigger = {
-//   trigger: marqueeContainer,
-//   start: "0 100%", // Start the trigger when the top of the container is 100% visible
-//   end: `+=${marqueeContentWidth}`, // End the trigger when the bottom of the container is 500px below the top
-//   onEnter: () => animationTimeline.play(0), // Restart the animation from the beginning when it enters the trigger
-//   onLeaveBack: () => animationTimeline.pause(0), // Pause the animation when it leaves the trigger
-// };
-
-// gsap.timeline().add(marqueeTrigger); // Add the scroll trigger to your timeline
+ScrollTrigger.create({
+  trigger: ".workButton",
+  pin: true,
+  end: "bottom 0%",
+});
